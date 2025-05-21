433295650b7fc4cea28311b81728be87a9804674
Johan <johanmp@outlook.fr>
2025-05-21 22:43:19 +0200

    Premier commit : creation des fichiers nécessaires et début de code en inline sur index.html

---
+++ easyps-arbitration-tool/src/index.js
@@ -0,0 +1,7 @@
+  
+  const startButton = document.getElementById('start-button');
+  if (startButton) {
+      startButton.addEventListener('click', function() {
+          window.location.href = 'Settings.html';
+      });
+  }

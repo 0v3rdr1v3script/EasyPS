a39884989434b3faeba89674a4328e4d90019624
PC portable <johanmp@outlook.fr>
2025-05-23 08:25:59 +0200

    suppression de index.js : code inline je changerai à nouveau après

--- index.js
+++
@@ -1,7 +0,0 @@
-  
-  const startButton = document.getElementById('start-button');
-  if (startButton) {
-      startButton.addEventListener('click', function() {
-          window.location.href = 'Settings.html';
-      });
-  }
